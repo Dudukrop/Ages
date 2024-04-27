3626e4d66a54e42de4935358c8101cba715f756c
Eduardo Kropniczki <dudukrop@gmail.com>
2024-04-27 11:41:04 -0300

    first commit

---
+++ Exercicio - Idades/Entities/Pessoa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio___Idades.Entities
+{
+    class Pessoa
+    {
+        public string Name1 { get; set; }
+        public int Age1 { get; set; }
+        public string Name2 { get; set; }
+        public int Age2 { get; set; }
+
+        public Pessoa(string name1, int age1, string name2, int age2)
+        {
+            Name1 = name1;
+            Age1 = age1;
+            Name2 = name2;
+            Age2 = age2;
+        }
+
+        public double Media()
+        {
+            return ((double)Age1 + Age2) / 2;
+        }
+
+        public override string ToString()
+        {
+            return "A idade media de "
+                + Name1
+                + " e "
+                + Name2
+                + " eh de "
+                + Media().ToString("F1", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio - Idades/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Exercicio___Idades.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio___Idades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("First person data:");
+            Console.Write("Name: ");
+            string name1 = Console.ReadLine();
+            Console.Write("Age: ");
+            int age1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Second person data:");
+            Console.Write("Name: ");
+            string name2 = Console.ReadLine();
+            Console.Write("Age: ");
+            int age2 = int.Parse(Console.ReadLine());
+
+            Pessoa pessoa = new Pessoa(name1, age1, name2, age2);
+
+            Console.WriteLine(pessoa);
+        }
+    }
+}
